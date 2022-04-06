d54cae77a24dc34d779c3522f8072f6389d1e979
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2022-04-06 21:00:47 +0000

    Update dockersetup.sh

--- dockersetup.sh
+++ dockersetup.sh
@@ -1,3 +1,3 @@
 ansible-galaxy install konstruktoid.hardening
 ansible-playbook -i localhost /docker-ubuntu-hardened/playbook.yml
-cd /U_RHEL_8_V1R5_STIG_Ansible/rhel8STIG-ansible/ && chmod +x ./enforce.sh && bash ./enforce.sh
+chmod +x /U_RHEL_8_V1R5_STIG_Ansible/rhel8STIG-ansible/enforce.sh && bash /U_RHEL_8_V1R5_STIG_Ansible/rhel8STIG-ansible/enforce.sh
